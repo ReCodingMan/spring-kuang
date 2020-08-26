73679238998c450951f2ed633cf65d22d5c57d28
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-26 20:02:21 +0800

    c命名空间和p命名空间注入

--- spring-study/spring-04-di/src/test/java/MyTest.java
+++ spring-study/spring-04-di/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.cc.pojo.Student;
+import com.cc.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,5 +10,12 @@ public class MyTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Student student = (Student) context.getBean("student");
         System.out.println(student.toString());
+    }
+
+    @Test
+    public void test() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans.xml");
+        User user = context.getBean("user2", User.class);
+        System.out.println(user);
     }
 }
