96797bb76ed4f831da3aa4dc66f75571cad27e0c
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-27 16:16:16 +0800

    动态代理理解

---
+++ spring-study/spring-08-proxy/src/main/java/com/cc/demo03/ProxyInvocationHandler.java
@@ -0,0 +1,51 @@
+package com.cc.demo03;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * 我们会用这个类，自动生成代理类
+ */
+public class ProxyInvocationHandler implements InvocationHandler {
+
+    //被代理的接口
+    private Rent rent;
+
+    public void setRent(Rent rent) {
+        this.rent = rent;
+    }
+
+    /**
+     * 生成得到代理类
+     * @return
+     */
+    public Object getProxy() {
+        return Proxy.newProxyInstance(this.getClass().getClassLoader(), rent.getClass().getInterfaces(), this);
+    }
+
+    /**
+     * 处理代理实例，并返回结果
+     * @param proxy
+     * @param method
+     * @param args
+     * @return
+     * @throws Throwable
+     */
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        //动态代理的本质，使用反射机制
+        seeHouse();
+        Object result = method.invoke(rent, args);
+        fare();
+        return result;
+    }
+
+    public void seeHouse() {
+        System.out.println("中介带你看房子");
+    }
+
+    public void fare() {
+        System.out.println("中介收中介费");
+    }
+}
