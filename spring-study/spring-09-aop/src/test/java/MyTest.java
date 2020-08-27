435ba1eff80397432137dd0e0fd642335b957e41
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-27 17:46:07 +0800

    AOP实现方式二

--- spring-study/spring-09-aop/src/test/java/MyTest.java
+++ spring-study/spring-09-aop/src/test/java/MyTest.java
@@ -1,11 +1,21 @@
 import com.cc.service.UserService;
 import com.cc.service.UserServiceImpl;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
 
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //动态代理代理的是接口：注意点
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.add();
+    }
+
+    @Test
+    public void test() {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         //动态代理代理的是接口：注意点
