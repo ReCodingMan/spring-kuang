9b9f12e535d02d5cc6a64a30fefc40b753e959dc
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-28 14:35:38 +0800

    回顾mybatis

---
+++ spring-study/spring-10-mybatis/src/main/java/com/cc/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.cc.mapper;
+
+import com.cc.pojo.User;
+
+import java.util.List;
+
+public interface UserMapper {
+
+    public List<User> selectUser();
+}
---
+++ spring-study/spring-10-mybatis/src/test/java/MyTest.java
@@ -0,0 +1,28 @@
+import com.cc.mapper.UserMapper;
+import com.cc.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MyTest {
+
+    @Test
+    public void test() throws IOException {
+        InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
+        SqlSession sqlSession = sessionFactory.openSession(true);
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = mapper.selectUser();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+    }
+}
